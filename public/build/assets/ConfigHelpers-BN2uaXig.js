4ed5ea2199bac5e138d127927c50148e1855271d
Anggit Utomo <pucukpisang97@gmail.com>
2024-11-26 23:04:54 +0700

    feat: :sparkles: alpha-1

--- public/build/assets/ConfigHelpers-BN2uaXig.js
+++
@@ -1,1 +0,0 @@
-class c{formatDate(r){const[e,t,n]=r.split("/");return`${n}-${t}-${e}`}formatCurrency(r){return parseInt(r).toLocaleString("id-ID",{style:"currency",currency:"IDR"})}}export{c as C};
