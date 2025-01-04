7aac0ca99bf0b46cb128155e560cd5b0cde94a32
Anggit Utomo <pucukpisang97@gmail.com>
2025-01-04 15:04:26 +0700

    feat: :sparkles: alpha-16 - fixed report chart

--- app/Http/Controllers/APP/ReportController.php
+++ app/Http/Controllers/APP/ReportController.php
@@ -24,51 +24,53 @@ class ReportController extends Controller
     private function filter()
     {
         $current_date = now();
-        $label_chart = [];
-        $total_amount = [];
+        $start_of_week = Carbon::parse($current_date)
+            ->copy()
+            ->startOfWeek(Carbon::MONDAY);
+        $end_of_weekdays = Carbon::parse($current_date)
+            ->copy()
+            ->endOfWeek(Carbon::SUNDAY);
 
-        $month_export = Carbon::now()->month;
-        $year_export = Carbon::now()->year;
-
-        $start_of_week = Carbon::parse($current_date)->copy()->startOfWeek(Carbon::MONDAY);
-        $end_of_weekdays = Carbon::parse($current_date)->copy()->endOfWeek(Carbon::SUNDAY);
-
+        // get days in the week
         $days = CarbonPeriod::create($start_of_week, $end_of_weekdays);
         $labels = [];
 
-        // Create an array to hold total amounts for expenses and income
         $totalAmounts = [
             'expense' => [],
             'income' => [],
         ];
 
+        // create days label
+        $day_name = [];
         foreach ($days as $day) {
+            $day_name[] = $day->format('Y-m-d');
             $labels[] = $day->format('d');
         }
 
-        // Fetch transactions for both types in a single query
-        $transactions = Transaction::selectRaw('DATE(transaction_date) as date, SUM(CASE WHEN type = "0" THEN amount ELSE 0 END) as total_expense, SUM(CASE WHEN type = "1" THEN amount ELSE 0 END) as total_income')
+        // get transactions for the week
+        $transactions = Transaction::selectRaw('
+            DATE(transaction_date) as date,
+            SUM(CASE WHEN type = "0" THEN amount ELSE 0 END) as total_expense,
+            SUM(CASE WHEN type = "1" THEN amount ELSE 0 END) as total_income
+        ')
             ->whereBetween('transaction_date', [$start_of_week, $end_of_weekdays])
             ->where('user_id', Auth::user()->id)
             ->groupBy('date')
-            ->get();
+            ->get()
+            ->keyBy('date');
 
-        // Initialize the totals for each day
-        foreach ($labels as $day) {
-            $formatted_day = $start_of_week->format('Y-m') . "-$day"; // Use current year and month
-            $transaction = collect($transactions)->firstWhere('date', $formatted_day);
+        foreach ($day_name as $day) {
+            $transaction = $transactions->get($day);
 
-            $totalAmounts['expense'][] = $transaction ? $transaction['total_expense'] : 0;
-            $totalAmounts['income'][] = $transaction ? $transaction['total_income'] : 0;
+            $totalAmounts['expense'][] = $transaction ? floatval($transaction->total_expense) : 0;
+            $totalAmounts['income'][] = $transaction ? floatval($transaction->total_income) : 0;
         }
-        $label_chart = $labels;
-        $month = $start_of_week->format('F');
 
         return [
-            'labels' => $label_chart,
-            "dataExpense" => $totalAmounts['expense'],
-            "dataIncome" => $totalAmounts['income'],
-            "monthName" => $month,
+            'labels' => $labels,
+            'dataExpense' => $totalAmounts['expense'],
+            'dataIncome' => $totalAmounts['income'],
+            'monthName' => $start_of_week->format('F'),
         ];
     }
 }
